b312b3e1e4f66a51678b3d9291942ace71c6a882
rongjoker <zhangshipeng1234@126.com>
2019-11-11 19:33:59 +0800

    test nio-buffer

---
+++ src/test/java/com/light/rain/test/BufferTest.java
@@ -0,0 +1,38 @@
+package com.light.rain.test;
+
+import org.junit.Test;
+
+import java.nio.ByteBuffer;
+
+public class BufferTest {
+
+
+    @Test
+    public void test1(){
+
+        ByteBuffer buffer = ByteBuffer.allocate(8);
+        buffer.clear();
+        byte[] bytes = new byte[]{1,2,3};
+
+        buffer.put(bytes);
+
+        buffer.flip();
+        System.out.println("buffer.remaining():"+buffer.remaining());
+
+        for (int i = 0; i <buffer.remaining(); i++) {
+
+            byte b = buffer.get(i);
+            System.out.println(i+":"+b);
+            System.out.println("buffer.remaining():"+buffer.remaining());
+        }
+
+
+        buffer.clear();
+
+
+
+
+
+
+    }
+}
