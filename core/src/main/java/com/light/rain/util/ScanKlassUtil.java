b42dbc9f5bee43f7869075716ccb966d189366db
rongjoker <zhangshipeng1234@126.com>
2019-12-23 00:44:39 +0800

    添加扫描

--- core/src/main/java/com/light/rain/util/ScanKlassUtil.java
+++ core/src/main/java/com/light/rain/util/ScanKlassUtil.java
@@ -1,29 +1,97 @@
 package com.light.rain.util;
 
+import lombok.extern.log4j.Log4j2;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
+@Log4j2
 public class ScanKlassUtil {
 
+    public static void main(String[] args) throws IOException {
+        ScanKlassUtil scanKlassUtil = new ScanKlassUtil();
+        scanKlassUtil.scanCandidateComponents("com.light.rain.util");
+
+    }
     private static final char PACKAGE_SEPARATOR = '.';
 
     private static final char PATH_SEPARATOR = '/';
 
     String CLASSPATH_ALL_URL_PREFIX = "classpath*:";
 
+    private final String DEFAULT_RESOURCE_PATTERN = "**/*.class";
 
-    public static String convertClassNameToResourcePath(String className) {
-        return className.replace(PACKAGE_SEPARATOR, PATH_SEPARATOR);
+
+    public static String resolveBasePackage(String basePackage) {
+        return basePackage.replace(PACKAGE_SEPARATOR, PATH_SEPARATOR);
     }
+
+    protected Resource convertClassLoaderURL(URL url) {
+        return new UrlResource(url);
+    }
+
+    protected Set<Resource> doFindAllClassPathResources(String path) throws IOException {
+        Set<Resource> result = new LinkedHashSet<>(16);
+        ClassLoader cl = this.getClass().getClassLoader();
+        Enumeration<URL> resourceUrls = (cl != null ? cl.getResources(path) : ClassLoader.getSystemResources(path));
+        while (resourceUrls.hasMoreElements()) {
+            URL url = resourceUrls.nextElement();
+            log.info("url:[{}]",url);
+            result.add(convertClassLoaderURL(url));
+
+        }
+
+        return result;
+    }
+
+
+    protected Set<Resource> doFindMatchingFileSystemResources(File rootDir, String subPattern) throws IOException {
+
+        Set<File> matchingFiles = retrieveMatchingFiles(rootDir, subPattern);
+        Set<Resource> result = new LinkedHashSet<>(matchingFiles.size());
+        for (File file : matchingFiles) {
+            result.add(new FileSystemResource(file));
+        }
+        return result;
+    }
+
 
 
     private Set<Class> scanCandidateComponents(String basePackage){
 
+        String rootDirPath = CLASSPATH_ALL_URL_PREFIX +
+                resolveBasePackage(basePackage);
 
-//        String packageSearchPath = CLASSPATH_ALL_URL_PREFIX +
-//                resolveBasePackage(basePackage) + '/' + this.resourcePattern;
+
+        String packageSearchPath = CLASSPATH_ALL_URL_PREFIX +
+                resolveBasePackage(basePackage) + '/' + DEFAULT_RESOURCE_PATTERN;
 //        Resource[] resources = getResourcePatternResolver().getResources(packageSearchPath);
-//
 
+        try {
+            String locationPattern = rootDirPath.substring("classpath*:".length());
+            log.info("locationPattern:[{}]",locationPattern);
+            Set<Resource> root = this.doFindAllClassPathResources(locationPattern);
+
+            root.forEach(resource -> {
+
+                try {
+                    log.info("resource.getFile:[{}]",resource.getFile());
+                } catch (IOException | URISyntaxException e) {
+                    e.printStackTrace();
+                }
+
+
+            });
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
         return null;
---
+++ core/src/main/java/com/light/rain/util/UrlResource.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.light.rain.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.annotation.Nullable;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+/**
+ * {@link Resource} implementation for {@code java.net.URL} locators.
+ * Supports resolution as a {@code URL} and also as a {@code File} in
+ * case of the {@code "file:"} protocol.
+ *
+ * @author Juergen Hoeller
+ * @since 28.12.2003
+ * @see URL
+ */
+public class UrlResource implements Resource {
+
+	private final URI uri;
+
+	private final URL url;
+
+	public static URI toURI(URL url) throws URISyntaxException {
+		return toURI(url.toString());
+	}
+
+	public static URI toURI(String location) throws URISyntaxException {
+		return new URI(StringUtils.replace(location, " ", "%20"));
+	}
+
+
+	/**
+	 * Create a new {@code UrlResource} based on the given URI object.
+	 * @param uri a URI
+	 * @throws MalformedURLException if the given URL path is not valid
+	 * @since 2.5
+	 */
+	public UrlResource(URI uri) throws MalformedURLException {
+		this.uri = uri;
+		this.url = uri.toURL();
+	}
+
+	/**
+	 * Create a new {@code UrlResource} based on the given URL object.
+	 * @param url a URL
+	 */
+	public UrlResource(URL url) {
+		this.url = url;
+		this.uri = null;
+	}
+
+	@Override
+	public boolean exists() {
+		return false;
+	}
+
+	@Override
+	public URL getURL() throws IOException {
+		return null;
+	}
+
+	@Override
+	public URI getURI() throws IOException {
+		return null;
+	}
+
+	@Override
+	public File getFile() throws URISyntaxException,IOException {
+
+		return new File(toURI(url).getSchemeSpecificPart());
+	}
+
+	@Override
+	public long contentLength() throws IOException {
+		return 0;
+	}
+
+	@Override
+	public long lastModified() throws IOException {
+		return 0;
+	}
+
+	@Override
+	public Resource createRelative(String relativePath) throws IOException {
+		return null;
+	}
+
+	@Nullable
+	@Override
+	public String getFilename() {
+		return null;
+	}
+
+	@Override
+	public String getDescription() {
+		return null;
+	}
+
+	@Override
+	public InputStream getInputStream() throws IOException {
+		return null;
+	}
+}
