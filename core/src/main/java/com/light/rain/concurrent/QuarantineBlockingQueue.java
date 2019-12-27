3c6d1084d92592809c9180744ad633c72f7b1101
rongjoker <zhangshipeng1234@126.com>
2019-12-27 20:59:00 +0800

    添加注释

--- core/src/main/java/com/light/rain/concurrent/QuarantineBlockingQueue.java
+++ core/src/main/java/com/light/rain/concurrent/QuarantineBlockingQueue.java
@@ -3,6 +3,11 @@ package com.light.rain.concurrent;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 
+/**
+ * LinkedBlockingQueue配合ThreadPoolExecutor使用，适合cpu密集型的并发作业，
+ * 因为队列容量是Integer.MAX,所以理论上只能开启corePoolSize个线，程无法激发maximumPoolSize
+ * QuarantineBlockingQueue调整了逻辑，探测threadPoolExecutor的线程大小，能够开启maximumPoolSize个线程
+ */
 public class QuarantineBlockingQueue extends LinkedBlockingQueue<Runnable> {
 
     private volatile ThreadPoolExecutor threadPoolExecutor;
